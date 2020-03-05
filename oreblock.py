b102d365ccaf1a369b4baf75ca948ec9ebb3c14d
Ellie Hetland <elaina.hetland@gmail.com>
2020-03-05 13:43:31 -0600

    implemented logic to cycle through colors using button

--- oreblock.py
+++ oreblock.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 import unicornhat as ore
 import time
+import signal
+import buttonshim
 
+
+@buttonshim.on_press(buttonshim.BUTTON_B)
+def button_a(button, pressed):
+    global current_index
+    current_index = current_index + 1
+    print(" Button press detected [{}][{}]".format(current_index,len(colors)))
+    if current_index >= len(colors):
+        #print(" end reached, reset to off")
+        current_index = -1
+
+    if current_index == -1:
+        #print("  off")
+        ore.off()
+    else:
+        #print("  selecting color for display")
+        current_color = colors[current_index]
+        ore.set_all(current_color)
+        ore.show()
+
+
+print "Starting Minecraft Ore Lamp - Button Mode"
 ore.set_layout(ore.AUTO)
 ore.rotation(0)
-ore.brightness(0.2)
+ore.brightness(0.4)
 
 DIAMOND = (69, 172, 165)
 GOLD = (254, 254, 63)
@@ -23,10 +46,11 @@ colors = [
     LAPIS_LAZUL  , # lapis lazul
     IRON  # iron   
 ] 
-ore.clear()
 
-for color in colors:
-    ore.set_all(color)
-    ore.show()
-    time.sleep(10)
+current_index = -1
+
+ore.clear()
+signal.pause()
+
+ore.off()
 
